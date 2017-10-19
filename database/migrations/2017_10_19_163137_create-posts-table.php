6d92337962478064c51f181a83b5fe74fcbef977
luisfer85 <luisfer85@gmail.com>
2017-10-19 21:25:22 +0200

    Soluciono errores tipograficos del modelo y la migracion

--- database/migrations/2017_10_19_163137_create-posts-table.php
+++ database/migrations/2017_10_19_163137_create-posts-table.php
@@ -15,7 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('tittle', 255);
+            $table->string('title', 255);
             $table->text('body');
             $table->timestamps();
         });
