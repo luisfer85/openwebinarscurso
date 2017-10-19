090081c3df6cf8eda6613190d95913aabae04b56
luisfer85 <luisfer85@gmail.com>
2017-10-19 17:59:35 +0200

    Implementamos la validacion para el futuro guardado de noticias

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
 
 class PostController extends Controller
 {
@@ -14,5 +15,21 @@ class PostController extends Controller
     public function show($id)
     {
     	return "Showing post " . $id;
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' ==> 'required|unique:posts|max:255',
+            'body' ==> 'required',
+        ]);
+
+        if($validator->fails()) {
+            return redirect('post/create')
+            ->withErrors($validator)
+            ->withInput();
+        }
+
+        //Store post
     }
 }
