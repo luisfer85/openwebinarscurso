509cee573590c416557f77573d493cf3091bc514
luisfer85 <luisfer85@gmail.com>
2017-10-20 13:46:49 +0200

    Creamos la plantilla que muestra todos los titulos de los posts

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -4,5 +4,20 @@
     </head>
     <body>
         <h1>Mi primera plantilla</h1>
+
+        @if (count($posts) > 0)
+            <h4>Listado de Posts:</h4>
+            <ul>
+                @foreach ($posts as $post)
+                    <li>{{ $post->title }}</li>
+                @endforeach
+            </ul>
+        @else
+            <h4>No hay Posts</h4>
+        @endif
+
+
+
+
     </body>
 </html>
